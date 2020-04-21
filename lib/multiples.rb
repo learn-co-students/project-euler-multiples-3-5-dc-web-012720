eb316fc4610bc1572f25b767882a21593c05540a
laurenyz <laurenkyu@gmail.com>
2020-04-21 09:31:23 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,14 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    multiples = []
+    (1...limit).each do |num|
+        if num % 3 == 0 || num % 5 == 0
+            multiples << num
+        end
+    end
+    multiples.uniq
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end
